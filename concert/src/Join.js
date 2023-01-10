fc031944c661be8e69fb99fdcf809af87d4659b1
이휘경 <ter0000@naver.com>
2023-01-10 22:49:18 +0900

    회원가입 형식추가

--- concert/src/Join.js
+++ concert/src/Join.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { type } from '@testing-library/user-event/dist/type';
+import { useCallback, useState } from 'react';
 import './css/Join.css'
+
 function Join() {
 
     const [email, setEmail] = useState(""); //이메일 input value 값을 저장할 state
@@ -14,7 +16,8 @@ function Join() {
     const [matchEmail, setMatchEmail] = useState(true); //이메일 input이 이메일 형식에 맞는지
     const [matchPassword, setMatchPassword]  = useState(true); //비밀번호가 비밀번호 형식에 맞는지
     const [samePassword, setSamePassword] = useState(true); //비밀번호와 비밀번호 확인 값이 같은지
-
+    const [numberKeyPress , setNumberKeyPress] = useState(false); //키보드  숫자 자판 눌렀을 
+    
     const onChangeEmail = (event) => {
         setEmail(event.target.value);
     }
@@ -35,16 +38,29 @@ function Join() {
         setName(event.target.value);
     }
 
-    const onChangePhoneNumber = (event) => {
-        setPhoneNumber(event.target.value);
+    
+    const onNumberKeyDown = (event) => { //숫자키패드 눌렀을 때 keydown 이벤트가 chagne 이벤트보다 빨리 일어나는 것을 이용함
+        if((!isNaN(event.key) && event.key !== " ") || event.key ==="Backspace") 
+            setNumberKeyPress(true);
+        else
+            setNumberKeyPress(false);
     }
 
+
+    const onChangePhoneNumber = (event) => {
+        if(numberKeyPress === true && event.target.value.length <= 11)
+            setPhoneNumber(event.target.value);
+    }
+
+
     const onChangeCheckPhoneNumber = (event) => {
-        setCheckPassword(event.target.value);
+        if(numberKeyPress === true && event.target.value.length <= 6)
+            setCheckPhoneNumber(event.target.value);
     }
     
     const onChangeBirth = (event) => {
-        setBirth(event.target.value);
+        if(numberKeyPress === true && event.target.value.length <= 8)
+            setBirth(event.target.value);
     }
 
     const onChangeGender = (event) => {
@@ -81,6 +97,52 @@ function Join() {
     const checkNickname = (e) => { //db check 필요
         e.preventDefault();
     }
+
+    const submitJoin = (e) =>{
+        e.preventDefault();
+        if(email ===  "") { //이메일이 공백이면
+            alert("이메일을 입력하세요.");
+        }
+        else if(matchEmail === false){ //이메일이 형식에 맞지 않으면
+            alert("이메일을 확인해주세요.")
+        }
+        else if(password === ""){
+            alert("비밀번호를 입력하세요.")
+        }
+        else if(matchPassword === false){
+            alert("비밀번호를 확인해주세요.")
+        }
+        else if(checkPassword === ""){
+            alert("비밀번호 확인을 입력해주세요.")
+        }
+        else if (samePassword === false){
+            alert("비밀번호가 일치하지 않습니다.")
+        }
+        else if(nickname === ""){
+            alert("닉네임을 입력해주세요.")
+        }
+        else if(name === ""){
+            alert("이름을 입력해주세요.")
+        }
+        else if(phoneNumber === "" || phoneNumber.length !== 11){
+            alert("휴대번호를 입력해주세요")
+        }
+        else if(checkPhoneNumber === "" || checkPhoneNumber.length !== 6){
+            alert("인증번호를 입력해주세요")
+        }
+        else if(birth === "" || birth.length !== 8){
+            alert("생년월일 입력해주세요")
+        }
+        else if(gender === ""){
+            alert("성별을 입력해주세요")
+        }
+        else{
+            console.log("api연결")
+        }
+
+
+        console.log(e);
+    }
     /*
        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
        RFC5322를 만족하는 JS 정규식을 갖고옴
@@ -93,6 +155,46 @@ function Join() {
         6:
     */
 
+
+
+
+    /*const phoneAuth = (e) => {
+        e.preventDefault();
+        const IMP = window.IMP;
+        const identification = process.env.REACT_APP_PHONE_IDENTIFICATION
+        IMP.init(identification);
+        
+        const data = {
+            merchant_uid: `mid_${new Date().getTime()}` , // 주문번호
+            company: '내한공연',                           // 회사명 또는 URL
+            carrier: 'SKT',                              // 통신사
+            name: name,                                // 이름
+            phone: phoneNumber,                        // 전화번호
+        };
+       
+        IMP.certification(data,callback)
+
+        function callback(response) {
+            const {
+                success,
+                merchant_uid,
+                error_msg
+            } = response;
+
+            if(success){
+                alert('본인인증 성공');
+            }else{
+                alert(`본인 인증 실패 : ${error_msg}`);
+            }
+        }
+    }
+    */
+
+    const phoneAuth = (e) => {  
+        e.preventDefault();
+        console.log(phoneNumber)
+
+    }
 
     return (
         <div className="joinWrap">
@@ -164,16 +266,20 @@ function Join() {
                 <input
                  type = "text"
                  id = "phoneInput"
+                 onKeyDown={onNumberKeyDown}
                  onChange = {onChangePhoneNumber}
+                 value = {phoneNumber}
                  placeholder = {"- 없이 입력하세요."}></input>
-                 <button className = {"checkBtn"} type = "submit" onClick = {checkNickname}>인증번호 전송</button>
+                 <button className = {"checkBtn"} onClick = {phoneAuth}>인증번호 전송</button>
             </div>
             <div className = "joinInput">
                 <em>* </em>
                 <label htmlFor = "phoneCheckInput">인증번호</label>
                 <input type = "text"
                  id = "phoneCheckInput"
-                 onChange={onChangeCheckPhoneNumber}
+                 onKeyDown={onNumberKeyDown}
+                 onChange = {onChangeCheckPhoneNumber}
+                 value = {checkPhoneNumber} 
                  placeholder ={"인증번호를 입력하세요."}></input>
             </div>
             <div className = "joinInput">
@@ -182,7 +288,9 @@ function Join() {
                 <input
                  type = "text"
                  id = "birthInput"
+                 value = {birth}
                  onChange={onChangeBirth}
+                 onKeyDown={onNumberKeyDown}
                  placeholder = {"8자리를 입력하세요."}></input>
             </div>
             <div className = "joinInput">
@@ -194,7 +302,7 @@ function Join() {
             <div className='terms'>
                 회원가입 약관
             </div>
-                <button type = "submit" className = "join" >회원가입</button>
+                <button type = "submit" className = "join" onClick={submitJoin} >회원가입</button>
           </form>
         </div>
     );
---
+++ concert/src/PhoneAuth.js
@@ -0,0 +1,17 @@
+
+function PhoneAuth() {
+    return (
+        <div className="nav">
+          <ul>
+            <li>NEWS</li>
+            <li>CONCERT</li>
+            <li>MUSIC</li>
+            <li>EVENT</li>
+            <li>COMMUNTIY</li>
+            <li>Disabled</li>
+          </ul>
+        </div>
+    );
+  }
+  
+  export default PhoneAuth;
